8d67a38a5f5d1551a6309a1bb01d890275a6911d
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2021-12-06 20:00:36 +0200

    First commit

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.join(__dirname, './src/client/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './src/client/views/index.html'),
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
